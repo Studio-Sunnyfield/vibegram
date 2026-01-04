fa8a323832e62616a3341e8a3c99cfde42d567e7
lenhsherr <lenhsherr@gmail.com>
2026-01-04 16:43:29 -0500

    Add code signing and GitHub Actions release workflow

---
+++ app/notarize.js
@@ -0,0 +1,22 @@
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  const appPath = `${appOutDir}/${appName}.app`;
+
+  console.log(`Notarizing ${appPath}...`);
+
+  await notarize({
+    appPath,
+    appleApiKey: process.env.APPLE_API_KEY,
+    appleApiKeyId: process.env.APPLE_API_KEY_ID,
+    appleApiIssuer: process.env.APPLE_API_ISSUER,
+  });
+
+  console.log('Notarization complete!');
+};
